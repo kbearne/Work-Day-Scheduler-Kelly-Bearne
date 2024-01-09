b0a45297bc592e972c2d8e6d0c96810b2a4ab083
Kelly <{ID}+{username}@users.noreply.github.com>
2024-01-09 13:04:50 +0000

    working on JS functionality to colour time blocks depending on past, present or future value

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,34 @@ Persist events between refreshes of a page */
 // display the current day at the top of the calendar when a user opens the planner
 const today = dayjs();
 $("#currentDay").text(today.format("dddd D MMM YYYY"));
+
+// colour code when clicked (event listener):
+/* past
+present
+future
+
+onload
+
+check against current date/time
+if less = past
+if equal = present
+if more = future
+
+get current date time */
+
+// when the page is ready
+document.addEventListener('DOMContentLoaded', function () {
+
+    // select all hour blocks
+    const hourBlocks = document.querySelectorAll('.hour');
+
+    // loop all hour blocks and check against current time
+    hourBlocks.forEach(function(hourBlock) {
+        // get the hour block value
+        let hourBlockValue = hourBlock.innerHTML;
+
+        // compare hour block value against current time
+        // TODO 
+    });
+
+});
